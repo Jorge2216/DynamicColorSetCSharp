294a2d9540ceb361e4c6d13c5a5d4e2534ad14e2
Jorgito <Jorgito@DESKTOP-8RTP0DC>
2022-09-23 09:53:00 -0300

    Add project files.

---
+++ DynamicColorSetCSharp/Form1.Designer.cs
@@ -0,0 +1,281 @@
+﻿
+namespace DynamicColorSetCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmdBorrar = new System.Windows.Forms.Button();
+            this.cmdCalc = new System.Windows.Forms.Button();
+            this.txtHasta = new System.Windows.Forms.TextBox();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.txtDesde = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.cmdEnd = new System.Windows.Forms.Button();
+            this.OF5 = new System.Windows.Forms.RichTextBox();
+            this.OF4 = new System.Windows.Forms.RichTextBox();
+            this.OF3 = new System.Windows.Forms.RichTextBox();
+            this.OF2 = new System.Windows.Forms.RichTextBox();
+            this.OF1 = new System.Windows.Forms.RichTextBox();
+            this.OF9 = new System.Windows.Forms.RichTextBox();
+            this.OF8 = new System.Windows.Forms.RichTextBox();
+            this.OF7 = new System.Windows.Forms.RichTextBox();
+            this.OF6 = new System.Windows.Forms.RichTextBox();
+            this.OF10 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // cmdBorrar
+            // 
+            this.cmdBorrar.Location = new System.Drawing.Point(1024, 77);
+            this.cmdBorrar.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdBorrar.Name = "cmdBorrar";
+            this.cmdBorrar.Size = new System.Drawing.Size(81, 37);
+            this.cmdBorrar.TabIndex = 12;
+            this.cmdBorrar.Text = "Borrar";
+            this.cmdBorrar.UseVisualStyleBackColor = true;
+            this.cmdBorrar.Click += new System.EventHandler(this.cmdBorrar_Click);
+            // 
+            // cmdCalc
+            // 
+            this.cmdCalc.Location = new System.Drawing.Point(1024, 16);
+            this.cmdCalc.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdCalc.Name = "cmdCalc";
+            this.cmdCalc.Size = new System.Drawing.Size(81, 37);
+            this.cmdCalc.TabIndex = 11;
+            this.cmdCalc.Text = "OK";
+            this.cmdCalc.UseVisualStyleBackColor = true;
+            this.cmdCalc.Click += new System.EventHandler(this.cmdCalc_Click);
+            // 
+            // txtHasta
+            // 
+            this.txtHasta.Location = new System.Drawing.Point(889, 17);
+            this.txtHasta.Margin = new System.Windows.Forms.Padding(4);
+            this.txtHasta.Name = "txtHasta";
+            this.txtHasta.Size = new System.Drawing.Size(81, 20);
+            this.txtHasta.TabIndex = 10;
+            this.txtHasta.Text = "OF10";
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(819, 28);
+            this.Label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(35, 13);
+            this.Label2.TabIndex = 9;
+            this.Label2.Text = "Hasta";
+            // 
+            // txtDesde
+            // 
+            this.txtDesde.Location = new System.Drawing.Point(706, 17);
+            this.txtDesde.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDesde.Name = "txtDesde";
+            this.txtDesde.Size = new System.Drawing.Size(79, 20);
+            this.txtDesde.TabIndex = 8;
+            this.txtDesde.Text = "OF1";
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(630, 28);
+            this.Label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(38, 13);
+            this.Label1.TabIndex = 7;
+            this.Label1.Text = "Desde";
+            // 
+            // cmdEnd
+            // 
+            this.cmdEnd.Location = new System.Drawing.Point(988, 472);
+            this.cmdEnd.Margin = new System.Windows.Forms.Padding(4);
+            this.cmdEnd.Name = "cmdEnd";
+            this.cmdEnd.Size = new System.Drawing.Size(118, 59);
+            this.cmdEnd.TabIndex = 13;
+            this.cmdEnd.Text = "Salir";
+            this.cmdEnd.UseVisualStyleBackColor = true;
+            this.cmdEnd.Click += new System.EventHandler(this.cmdEnd_Click);
+            // 
+            // OF5
+            // 
+            this.OF5.BackColor = System.Drawing.Color.White;
+            this.OF5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF5.Location = new System.Drawing.Point(26, 104);
+            this.OF5.Margin = new System.Windows.Forms.Padding(4);
+            this.OF5.Name = "OF5";
+            this.OF5.Size = new System.Drawing.Size(127, 67);
+            this.OF5.TabIndex = 18;
+            this.OF5.Text = "OF5";
+            // 
+            // OF4
+            // 
+            this.OF4.BackColor = System.Drawing.Color.White;
+            this.OF4.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF4.Location = new System.Drawing.Point(26, 194);
+            this.OF4.Margin = new System.Windows.Forms.Padding(4);
+            this.OF4.Name = "OF4";
+            this.OF4.Size = new System.Drawing.Size(127, 67);
+            this.OF4.TabIndex = 17;
+            this.OF4.Text = "OF4";
+            // 
+            // OF3
+            // 
+            this.OF3.BackColor = System.Drawing.Color.White;
+            this.OF3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF3.Location = new System.Drawing.Point(26, 282);
+            this.OF3.Margin = new System.Windows.Forms.Padding(4);
+            this.OF3.Name = "OF3";
+            this.OF3.Size = new System.Drawing.Size(127, 67);
+            this.OF3.TabIndex = 16;
+            this.OF3.Text = "OF3";
+            // 
+            // OF2
+            // 
+            this.OF2.BackColor = System.Drawing.Color.White;
+            this.OF2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF2.Location = new System.Drawing.Point(26, 369);
+            this.OF2.Margin = new System.Windows.Forms.Padding(4);
+            this.OF2.Name = "OF2";
+            this.OF2.Size = new System.Drawing.Size(127, 67);
+            this.OF2.TabIndex = 15;
+            this.OF2.Text = "OF2";
+            // 
+            // OF1
+            // 
+            this.OF1.BackColor = System.Drawing.Color.White;
+            this.OF1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF1.Location = new System.Drawing.Point(26, 464);
+            this.OF1.Margin = new System.Windows.Forms.Padding(4);
+            this.OF1.Name = "OF1";
+            this.OF1.Size = new System.Drawing.Size(127, 67);
+            this.OF1.TabIndex = 14;
+            this.OF1.Text = "OF1";
+            // 
+            // OF9
+            // 
+            this.OF9.BackColor = System.Drawing.Color.White;
+            this.OF9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF9.Location = new System.Drawing.Point(618, 104);
+            this.OF9.Margin = new System.Windows.Forms.Padding(4);
+            this.OF9.Name = "OF9";
+            this.OF9.Size = new System.Drawing.Size(127, 67);
+            this.OF9.TabIndex = 19;
+            this.OF9.Text = "OF9";
+            // 
+            // OF8
+            // 
+            this.OF8.BackColor = System.Drawing.Color.White;
+            this.OF8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF8.Location = new System.Drawing.Point(470, 104);
+            this.OF8.Margin = new System.Windows.Forms.Padding(4);
+            this.OF8.Name = "OF8";
+            this.OF8.Size = new System.Drawing.Size(127, 67);
+            this.OF8.TabIndex = 20;
+            this.OF8.Text = "OF8";
+            // 
+            // OF7
+            // 
+            this.OF7.BackColor = System.Drawing.Color.White;
+            this.OF7.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF7.Location = new System.Drawing.Point(322, 104);
+            this.OF7.Margin = new System.Windows.Forms.Padding(4);
+            this.OF7.Name = "OF7";
+            this.OF7.Size = new System.Drawing.Size(127, 67);
+            this.OF7.TabIndex = 21;
+            this.OF7.Text = "OF7";
+            // 
+            // OF6
+            // 
+            this.OF6.BackColor = System.Drawing.Color.White;
+            this.OF6.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF6.Location = new System.Drawing.Point(176, 104);
+            this.OF6.Margin = new System.Windows.Forms.Padding(4);
+            this.OF6.Name = "OF6";
+            this.OF6.Size = new System.Drawing.Size(127, 67);
+            this.OF6.TabIndex = 22;
+            this.OF6.Text = "OF6";
+            // 
+            // OF10
+            // 
+            this.OF10.BackColor = System.Drawing.Color.White;
+            this.OF10.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.OF10.Location = new System.Drawing.Point(772, 104);
+            this.OF10.Margin = new System.Windows.Forms.Padding(4);
+            this.OF10.Name = "OF10";
+            this.OF10.Size = new System.Drawing.Size(127, 67);
+            this.OF10.TabIndex = 23;
+            this.OF10.Text = "OF10";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1119, 555);
+            this.Controls.Add(this.OF10);
+            this.Controls.Add(this.OF6);
+            this.Controls.Add(this.OF7);
+            this.Controls.Add(this.OF8);
+            this.Controls.Add(this.OF9);
+            this.Controls.Add(this.OF5);
+            this.Controls.Add(this.OF4);
+            this.Controls.Add(this.OF3);
+            this.Controls.Add(this.OF2);
+            this.Controls.Add(this.OF1);
+            this.Controls.Add(this.cmdEnd);
+            this.Controls.Add(this.cmdBorrar);
+            this.Controls.Add(this.cmdCalc);
+            this.Controls.Add(this.txtHasta);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.txtDesde);
+            this.Controls.Add(this.Label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button cmdBorrar;
+        internal System.Windows.Forms.Button cmdCalc;
+        internal System.Windows.Forms.TextBox txtHasta;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.TextBox txtDesde;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button cmdEnd;
+        internal System.Windows.Forms.RichTextBox OF5;
+        internal System.Windows.Forms.RichTextBox OF4;
+        internal System.Windows.Forms.RichTextBox OF3;
+        internal System.Windows.Forms.RichTextBox OF2;
+        internal System.Windows.Forms.RichTextBox OF1;
+        internal System.Windows.Forms.RichTextBox OF9;
+        internal System.Windows.Forms.RichTextBox OF8;
+        internal System.Windows.Forms.RichTextBox OF7;
+        internal System.Windows.Forms.RichTextBox OF6;
+        internal System.Windows.Forms.RichTextBox OF10;
+    }
+}
---
+++ DynamicColorSetCSharp/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DynamicColorSetCSharp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdEnd_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmdCalc_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SetOrigen(txtDesde.Text);
+            }
+            catch (Exception ex)
+            {
+                                
+                Console.WriteLine(ex.Message.ToString());
+            }
+            try
+            {
+                SetDestino(txtHasta.Text);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+
+            }
+        }
+        private void SetOrigen(string Origen)
+        {
+            Form ParentForm;
+            ParentForm = this.FindForm();
+            var findTextBox = ParentForm.Controls.Find(Origen, true).FirstOrDefault();
+
+            if (findTextBox != null)
+            {
+                findTextBox.Enabled = true;
+                findTextBox.BackColor = Color.Aquamarine;
+            }
+        }
+        private void SetDestino(string Destino)
+        {
+            Form ParentForm;
+            ParentForm = this.FindForm();
+            var findTextBox = ParentForm.Controls.Find(Destino, true).FirstOrDefault();
+
+            if (findTextBox != null)
+            {
+                findTextBox.Enabled = true;
+                findTextBox.BackColor = Color.Aquamarine;
+            }
+        }
+
+        private void cmdBorrar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                ClearColors();
+            }
+            catch (Exception ex)
+            {                
+                Console.WriteLine(ex.Message.ToString());
+            }
+        }
+        private void ClearColors()
+        {
+            Form ParentForm;
+            ParentForm = this.FindForm();
+            foreach (Control OFN in ParentForm.Controls)
+            {                
+                String TipoControl = OFN.GetType().ToString();
+                if (TipoControl == "System.Windows.Forms.RichTextBox")
+                {
+                    OFN.BackColor = Color.White;
+                }
+            }
+
+        }
+    }
+}
